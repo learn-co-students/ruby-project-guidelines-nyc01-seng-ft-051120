8e168b9143bc31edcb3b542a25ad03620de70031
smatalha <sma.talha@gmail.com>
2020-05-27 15:21:54 -0400

    seeded partially

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Airline.delete_all
+
+jb = Airline.create(name: "Jet Blue")
+dt = Airline.create(name: "Delta")
+
+pas1 = Passenger.create(name: "Tony", address: "New York")
+pas2 = Passenger.create(name: "Conor", address: "New Jersey")
