b9071bc9379feeb09f584cbc72bdaea41f4d52c0
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-05-27 20:09:52 -0400

    set up run file

---
+++ lib/cli.rb
@@ -0,0 +1,71 @@
+
+class CommandLineInterface
+
+
+    def run
+        puts ""                                                                                                                     
+        puts "     _____                                    __   _   _            _____          __          ___                  _ "
+        puts "    / ____|                                  / _| | | | |          |_   _|         X X        / (_)                | |"
+        puts "   | |     __ ___   _____ _ __ _ __     ___ | |_  | |_| |__   ___    | |  ___ ___   X X  /X  / / _ ______ _ _ __ __| |"
+        puts "   | |    / _` X X / / _ X '__| '_ X   / _ X|  _| | __| '_ X / _ X   | | / __/ _ X   X X/  X/ / | |_  / _` | '__/ _` |"
+        puts "   | |___| (_| |X V /  __/ |  | | | | | (_) | |   | |_| | | |  __/  _| || (_|  __/    X  /X  /  | |/ / (_| | | | (_| |"
+        puts "    X_____X__,_| X_/ X___|_|  |_| |_|  X___/|_|    X__|_| |_|X___| |_____X___X___|     X/  X/   |_/___X__,_|_|  X__,_|"
+        puts ""
+        puts ""
+        puts ""
+        puts ""
+        puts "                                            /'X                                                         "
+        puts "                                           /   X              /'X       _                               "
+        puts "                                      /'.,/     X_         .,'   X     / X_                             "
+        puts "                                     /            X      _/       X_  /    X     _                      "
+        puts "                            X__,.   /              X    /           X/.,   _|  _/ X                     "
+        puts "                           /     X_/                X  /',.,''X      X_ X_/  X/    X                    "
+        puts "                           |                      _  X/   /    ',../',.X    _/      X                   "
+        puts "                          /         /           _XmX  X  /    |         X  /.,/'X   _X                  "
+        puts "                         /        _/           XMMmmX  X_     |          X/      X_/  X                 "
+        puts "                        /        /      X     XMMMMmmX   X__   X          X_       X   X_               "
+        puts "                       /        /        X   XMMMMMMmX      X   X           X       X    X              "
+        puts "                     _/        /          X  XMMMMMMmmX      X___            X_      X_   X             "
+        puts "                    /         /            XXMMMMMMMMmmX____.'  /X_            X       X   X_           "
+        puts "   ________________/_________/             /'.,___________...,,'   X            X   X        X________________________ "
+
+    end
+
+    def wizard
+        puts "               XX"
+        puts "                 XX"
+        puts "               _ XX"
+        puts "            .' . XX '."
+        puts "           '_ '_XX_'  `_"
+        puts "           .  . XX  .  ."
+        puts "          .  . XX  .  ."
+        puts "         .==. ` XX' .'"
+        puts "  .Y|   (|bd|)   X,"
+        puts "  Y_'`._(|__|)_.'`.;"
+        puts "    `.__      __,' XX"
+        puts "        |    |      XX"
+        puts "        |    |       `"
+        puts "        |    |"
+        puts "        |    |"
+        puts "        |____|"
+        puts "       =='  '=="
+    end
+        
+
+    def melt
+
+        puts " .------,"
+        puts ",'_____,'|"
+        puts "|    XX| |"
+        puts "|  o   | |"
+        puts "| )|(  | |"
+        puts "|  |   | |"
+        puts "| XX   | ;"
+        puts "|______|'._"
+        puts "   `._     `;"
+        puts "      `-._.'"
+                                                    
+    end
+        
+
+end
